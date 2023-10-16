6f035ec92a7847dd2af7f39731a0f95fa04b19f9
Zaid Sheikh <zaid.shaikh@cubixlabs.com>
2023-10-16 17:41:10 +0500

    Initial Commit

---
+++ CustomToggleSwiftUi/CustomToggleSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomToggleSwiftUiApp.swift
+//  CustomToggleSwiftUi
+//
+//  Created by Zaid Sheikh on 16/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct CustomToggleSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
