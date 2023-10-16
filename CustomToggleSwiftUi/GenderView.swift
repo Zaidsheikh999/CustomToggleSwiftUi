edd4072e2bdfae5867c9b3e750cc60f160413eef
Zaid Sheikh <zaid.shaikh@cubixlabs.com>
2023-10-16 17:46:23 +0500

    CustomToggleSwiftUi

---
+++ CustomToggleSwiftUi/GenderView.swift
@@ -0,0 +1,20 @@
+//
+//  GenderView.swift
+//  CustomToggleSwiftUi
+//
+//  Created by Zaid Sheikh on 16/10/2023.
+//
+
+import SwiftUI
+
+struct GenderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GenderView_Previews: PreviewProvider {
+    static var previews: some View {
+        GenderView()
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# CustomToggleSwiftUi
